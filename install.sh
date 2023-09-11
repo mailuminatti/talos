dc2e9d3c4500b38069dba04cd147731f367d41fd
mailuminatti <46273278+mailuminatti@users.noreply.github.com>
2023-09-10 23:25:23 -0400

    created install.sh file

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git clone https://github.com/mailuminatti/talos.git
+cd $(pwd)/talos
+git checkout talos-dev
+cd $(pwd)/cli
+echo "Installing Talos"
+pip3 install --editable .
+cd ../../
+chown -R $USER: talos
+echo "Cleaning up"
+rm -f -R talos
