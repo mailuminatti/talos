8f60ea73dce13939acb465e8979afcba90119fce
mailuminatti <46273278+mailuminatti@users.noreply.github.com>
2023-09-10 20:19:39 -0400

    added --version and dockerfile

--- cli/setup.py
+++ cli/setup.py
@@ -4,16 +4,17 @@ import os
 
 lib_folder = os.path.dirname(os.path.realpath(__file__))
 requirement_path = f"{ os.path.dirname(lib_folder)}/requirements.txt"
+version_path = f"{lib_folder}/version.py"
 install_requires = [] 
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
 
-
+exec(open(version_path).read())
 
 setup(
     name='talos',
-    version='0.1.0',
+    version=__version__, # type: ignore
     packages=find_packages(),
     install_requires=install_requires,
     entry_points={
--- cli/talos.py
+++ cli/talos.py
@@ -1,12 +1,13 @@
 import click
-
 from deploy import deploy
 from build import build
+from version import __version__ as cliversion
 
 # from service import commands as service
 
 @click.group()
-
+@click.pass_context
+@click.version_option(cliversion)
 def entry_point():
     pass
 
